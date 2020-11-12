a73d61a6090ace627a63fb4a46b2de5c711bfa7f
Lars Skovslund <lars.skovslund@hotmail.com>
2020-11-12 20:08:08 +0100

    Configured code analysis

--- test/Atc.Cosmos.EventStore.Tests/TestEvent.cs
+++ test/Atc.Cosmos.EventStore.Tests/TestEvent.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
-using Xunit;
-
-namespace BigBang.Cosmos.EventStore.Tests
+﻿namespace BigBang.Cosmos.EventStore.Tests
 {
     public class TestEvent
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = default!;
     }
 }
