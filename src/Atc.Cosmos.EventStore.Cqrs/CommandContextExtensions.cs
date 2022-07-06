1b15bff70d7de8104ee292e96c8ca02f6ae11124
LarsSkovslund <lars.skovslund@hotmail.com>
2022-07-06 09:24:57 +0200

    Introduce convenience extension methods to CommandContext

---
+++ src/Atc.Cosmos.EventStore.Cqrs/CommandContextExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Atc.Cosmos.EventStore.Cqrs
+{
+    public static class CommandContextExtensions
+    {
+        public static ICommandContext AddEventWhen(
+            this ICommandContext context,
+            Func<bool> condition,
+            Func<object> eventProvider)
+            => condition()
+             ? context.OnAddEvent(eventProvider())
+             : context;
+
+        public static ValueTask AsAsync(
+            this ICommandContext context)
+            => default; // default is a completed value task.
+
+        private static ICommandContext OnAddEvent(
+            this ICommandContext context,
+            object @event)
+        {
+            context.AddEvent(@event);
+
+            return context;
+        }
+    }
+}
