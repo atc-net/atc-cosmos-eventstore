56fb8d1ca454ea860c366e1009a415b2e633774b
Christian Helle <christian.helle@outlook.com>
2023-06-22 19:51:03 +0200

    Use ThrowIfEventLimitExceeded() in StreamBatch

---
+++ test/Atc.Cosmos.EventStore.Tests/StreamBatchTests.cs
@@ -0,0 +1,33 @@
+using Atc.Cosmos.EventStore.Events;
+using Atc.Test;
+using FluentAssertions;
+using Xunit;
+
+namespace Atc.Cosmos.EventStore.Streams.Tests;
+
+public class StreamBatchTests
+{
+    [Theory]
+    [InlineAutoNSubstituteData]
+    internal void Constructor_WithValidMetadataAndEvents_ReturnsInstance(
+        StreamMetadata metadata,
+        List<EventDocument> events)
+    {
+        var sut = new StreamBatch(metadata, events);
+        sut.Should().NotBeNull();
+        sut.Metadata.Should().BeEquivalentTo(metadata);
+        sut.Documents.Should().BeEquivalentTo(events);
+    }
+
+    [Theory]
+    [InlineAutoNSubstituteData]
+    internal void Constructor_WithEvents_ThrowsException_WhenEventLimitIsExceeded(
+        StreamMetadata metadata,
+        List<EventDocument> events)
+    {
+        var action = () => new StreamBatch(
+            metadata,
+            Enumerable.Repeat(events[0], 100).ToList());
+        action.Should().Throw<EventLimitExceededException>();
+    }
+}
