643b19436ae8799bb00d220e4afb74fa8ed192f4
Christian Helle <christian.helle@outlook.com>
2023-06-22 19:43:11 +0200

    Introduce ThrowIfEventLimitExceeded() extension method

---
+++ src/Atc.Cosmos.EventStore/Events/EventDocumentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Atc.Cosmos.EventStore.Cosmos;
+
+namespace Atc.Cosmos.EventStore.Events;
+
+internal static class EventDocumentExtensions
+{
+    public static IReadOnlyCollection<T> ThrowIfEventLimitExceeded<T>(
+        this IReadOnlyCollection<T> events)
+    {
+        if (events.Count > CosmosConstants.EventLimit)
+        {
+            throw new EventLimitExceededException(
+                events.Count,
+                CosmosConstants.EventLimit);
+        }
+
+        return events;
+    }
+}
