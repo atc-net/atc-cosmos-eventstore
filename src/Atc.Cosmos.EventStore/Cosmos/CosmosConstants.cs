2b8fdd8d2bc99637dd71172d73422ac91bef4c5d
Christian Helle <christian.helle@outlook.com>
2023-06-23 09:13:51 +0200

    Make CosmosConstants public

--- src/Atc.Cosmos.EventStore/Cosmos/CosmosConstants.cs
+++ src/Atc.Cosmos.EventStore/Cosmos/CosmosConstants.cs
@@ -1,16 +1,17 @@
 namespace Atc.Cosmos.EventStore.Cosmos;
 
-internal static class CosmosConstants
+public static class CosmosConstants
 {
     /// <summary>
     /// Cosmos DB has a limit of 100 operations per batch and a limit of 2MB per batch.
     /// See https://docs.microsoft.com/en-us/azure/cosmos-db/concepts-limits#per-request-limits.
     /// </summary>
-    internal const int BatchLimit = 100;
+    public const int BatchLimit = 100;
 
     /// <summary>
     /// A transaction will consist of at least 2 operations,
     /// one for the stream metadata and the other for the events.
+    /// Since there is no efficient way to determine the size of the total payload, 
     /// </summary>
-    internal const int EventLimit = BatchLimit - 1;
+    public const int EventLimit = BatchLimit / 2;
 }
