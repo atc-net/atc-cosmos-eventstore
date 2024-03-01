efddf590ad1930d9fc5b9d7bfef892ea81bbc26a
Christian Helle <christian.helle@outlook.com>
2024-03-01 08:52:08 +0100

    Add FooEventStreamId class

---
+++ sample/GettingStarted/FooEventStreamId.cs
@@ -0,0 +1,32 @@
+﻿using Atc.Cosmos.EventStore.Cqrs;
+
+namespace GettingStarted;
+
+public sealed class FooEventStreamId : EventStreamId, IEquatable<FooEventStreamId?>
+{
+    private const string TypeName = "foo";
+    public const string FilterIncludeAllEvents = TypeName + ".*";
+
+    public FooEventStreamId(string id)
+        : base(TypeName, id)
+    {
+        Id = id;
+    }
+
+    public FooEventStreamId(EventStreamId id)
+        : base(id.Parts.ToArray())
+    {
+        Id = id.Parts[1];
+    }
+
+    public string Id { get; }
+
+    public override bool Equals(object? obj)
+        => Equals(obj as FooEventStreamId);
+
+    public bool Equals(FooEventStreamId? other)
+        => other != null && Value == other.Value;
+
+    public override int GetHashCode()
+        => HashCode.Combine(Value);
+}
