4433f71c879efd633f4c861fa7c02abe509b9a5c
Christian Helle <christian.helle@outlook.com>
2024-03-01 08:51:56 +0100

    Add event classes for GettingStarted project

---
+++ sample/GettingStarted/Events/AddressChangedEvent.cs
@@ -0,0 +1,6 @@
+﻿using Atc.Cosmos.EventStore.Cqrs;
+
+namespace GettingStarted;
+
+[StreamEvent("address-changed-event:v1")]
+public record AddressChangedEvent(string OldAddress, string NewAddress);
