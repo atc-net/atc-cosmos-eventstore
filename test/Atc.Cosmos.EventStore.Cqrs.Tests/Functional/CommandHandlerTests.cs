18678f773b866f5b827c05b4b6352f3fb16a6bbf
Andreas Gehrke <ange@clever.dk>
2025-04-03 10:11:35 +0200

    Functional tests of Command execution and projections

---
+++ test/Atc.Cosmos.EventStore.Cqrs.Tests/Functional/CommandHandlerTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Atc.Cosmos.EventStore.Cqrs.Tests.Functional;
+
+#nullable enable
+
+[Trait("Category", "Functional")]
+public class CommandHandlerTests : IAsyncLifetime
+{
+    private CqrsTestHost host = null!;
+
+    [Fact]
+    public async Task Result_from_CommandHandler_must_be_returned()
+    {
+        var tick = DateTime.UtcNow.Ticks;
+        var commandResult = await host.Services.GetRequiredService<ICommandProcessorFactory>()
+            .Create<MakeTimeTickCommand>()
+            .ExecuteAsync(new MakeTimeTickCommand(tick), CancellationToken.None);
+
+        Assert.NotNull(commandResult);
+        Assert.NotNull(commandResult.Response);
+        Assert.Equal(tick, commandResult.Response);
+    }
+
+    [Fact]
+    public async Task CommandHandler_can_consume_existing_events()
+    {
+        // Produce some events by making time tick
+        await MakeTimeTick(host);
+        await MakeTimeTick(host);
+        await MakeTimeTick(host);
+
+        // Query events
+        var result = await host.Services.GetRequiredService<ICommandProcessorFactory>()
+            .Create<QueryTimeTickCommand>()
+            .ExecuteAsync(new QueryTimeTickCommand(), CancellationToken.None);
+
+        var events = Assert.IsType<List<(TimeTickedEvent Evt, EventMetadata Metadata)>>(result.Response);
+        Assert.Equal(3, events.Count);
+
+        static async Task MakeTimeTick(CqrsTestHost host)
+        {
+            var tick = DateTime.UtcNow.Ticks;
+            var commandProcessorFactory = host.Services.GetRequiredService<ICommandProcessorFactory>();
+            _ = await commandProcessorFactory
+                .Create<MakeTimeTickCommand>()
+                .ExecuteAsync(new MakeTimeTickCommand(tick), CancellationToken.None);
+        }
+    }
+
+    [Fact]
+    public async Task CommandHandler_that_consumes_events_works_when_no_existing_events_are_present()
+    {
+        // Query events - none exists
+        var result = await host.Services.GetRequiredService<ICommandProcessorFactory>()
+            .Create<QueryTimeTickCommand>()
+            .ExecuteAsync(new QueryTimeTickCommand(), CancellationToken.None);
+
+        var events = Assert.IsType<List<(TimeTickedEvent Evt, EventMetadata Metadata)>>(result.Response);
+        Assert.Empty(events);
+    }
+
+    [Fact]
+    public async Task Command_that_uses_RequiredVersion_Exists_must_result_in_NotFound_when_no_existing_events_are_present()
+    {
+        // Query events - must fail as non exists
+        var result = await host.Services.GetRequiredService<ICommandProcessorFactory>()
+            .Create<QueryExistingTimeTickCommand>()
+            .ExecuteAsync(new QueryExistingTimeTickCommand(), CancellationToken.None);
+
+        Assert.Equal(ResultType.NotFound, result.Result);
+    }
+
+    public async Task InitializeAsync()
+    {
+        host = new CqrsTestHost();
+        await host.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await host.DisposeAsync();
+    }
+}
---
+++ test/Atc.Cosmos.EventStore.Cqrs.Tests/Functional/Time.cs
@@ -0,0 +1,91 @@
+﻿namespace Atc.Cosmos.EventStore.Cqrs.Tests.Functional;
+
+[ProjectionFilter("*")]
+internal class TimeProjection(FakeDatabase fakeDatabase) : IConsumeEvent<TimeTickedEvent>, IProjection
+{
+    private long lastTick = 0;
+
+    public void Consume(TimeTickedEvent evt, EventMetadata metadata)
+    {
+        lastTick = evt.TickValue;
+    }
+
+    public Task InitializeAsync(EventStreamId id, CancellationToken cancellationToken)
+    {
+        // Here we could read latest "snapshot/view" value from DB
+        return Task.CompletedTask;
+    }
+
+    public Task CompleteAsync(CancellationToken cancellationToken)
+    {
+        // Persist "snapshot/view" value to DB
+        fakeDatabase.Save("TimeProjection", lastTick);
+
+        return Task.CompletedTask;
+    }
+
+    public Task<ProjectionAction> FailedAsync(Exception exception, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(ProjectionAction.Continue);
+    }
+}
+
+/// <summary>
+/// Queries all stored TimeTickedEvent.
+/// </summary>
+internal record QueryTimeTickCommand() : CommandBase<EventStreamId>(new EventStreamId("time"));
+
+/// <summary>
+/// Queries all stored TimeTickedEvent - at least one event must exist.
+/// </summary>
+internal record QueryExistingTimeTickCommand()
+    : CommandBase<EventStreamId>(new EventStreamId("time"), RequiredVersion: EventStreamVersion.Exists);
+
+internal class QueryTimeTickHandler :
+    ICommandHandler<QueryTimeTickCommand>,
+    ICommandHandler<QueryExistingTimeTickCommand>,
+    IConsumeEvent<TimeTickedEvent>
+{
+    private readonly List<(TimeTickedEvent Evt, EventMetadata Metadata)> events = new();
+
+    public void Consume(TimeTickedEvent evt, EventMetadata metadata)
+    {
+        events.Add((evt, metadata));
+    }
+
+    public ValueTask ExecuteAsync(
+        QueryTimeTickCommand command,
+        ICommandContext context,
+        CancellationToken cancellationToken)
+    {
+        context.ResponseObject = events;
+        return default;
+    }
+
+    public ValueTask ExecuteAsync(
+        QueryExistingTimeTickCommand command,
+        ICommandContext context,
+        CancellationToken cancellationToken)
+    {
+        context.ResponseObject = events;
+        return default;
+    }
+}
+
+internal record MakeTimeTickCommand(long Tick) : CommandBase<EventStreamId>(new EventStreamId("time"));
+
+internal class MakeTimeTickCommandHandler : ICommandHandler<MakeTimeTickCommand>
+{
+    public ValueTask ExecuteAsync(
+        MakeTimeTickCommand command,
+        ICommandContext context,
+        CancellationToken cancellationToken)
+    {
+        // Add event with Tick value from command
+        context.AddEvent(new TimeTickedEvent(command.Tick));
+        context.ResponseObject = command.Tick;
+        return default;
+    }
+}
+
+internal sealed record TimeTickedEvent(long TickValue);
