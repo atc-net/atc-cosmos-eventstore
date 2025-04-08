dc92f61cfce863bd02dc837177fd09ae3fc063cf
ATCBot <83698916+ATCBot@users.noreply.github.com>
2025-04-08 13:00:07 +0200

    Release of new minor version v1.20 (#69)
    * Update main with changes in stable after v1.17.0 release (#63)
    
    * Release of new minor version v1.16 (#61)
    
    * Set version to '1.16-preview'
    
    * Make sure that the projections are processed in a scoped application lifecycle
    
    * clean up scope
    
    * update changelog
    
    * The projection is not optional
    
    * Set version to '1.16'
    
    ---------
    
    Co-authored-by: Niels Lunn <nilu@clever.dk>
    
    * Release of new minor version v1.17 (#62)
    
    * Set version to '1.16-preview'
    
    * Make sure that the projections are processed in a scoped application lifecycle
    
    * clean up scope
    
    * update changelog
    
    * The projection is not optional
    
    * Set version to '1.16'
    
    * Set version to '1.17-preview'
    
    * ci: fix dotnet pack unable to find build output
    
    * Set version to '1.17'
    
    ---------
    
    Co-authored-by: Niels Lunn <nilu@clever.dk>
    Co-authored-by: Christian Helle <christian.helle@outlook.com>
    
    * Updated CHANGELOG.md for 1.17.0 release
    
    ---------
    
    Co-authored-by: Niels Lunn <nilu@clever.dk>
    Co-authored-by: Christian Helle <christian.helle@outlook.com>
    
    * Allow CommandHandlers to have scoped dependencies (#64)
    
    * Register ICommandProcessorFactory, ICommandHandlerFactory
    and CommandProcessor as transient
    to allow to CommandHandler to use scoped dependencies.
    
    * Update changelog
    
    ---------
    
    Co-authored-by: Andreas Gehrke <ange@clever.dk>
    
    * Set version to '1.18'
    
    * Set version to '1.19-preview'
    
    * Set version to '1.19'
    
    * Set version to '1.20-preview'
    
    * Bring back ProjectionFactory
    
    Also use IServiceScopeFactory instead of IServiceProvider in
    ProjectionProcessor to make it more clear that we are only creating scopes
    
    * Changelog
    
    * Functional tests of Command execution and projections
    
    * Upgrade SonarAnalyzer.CSharp as the current version reports bogus issues
    
    * Fix Warning S6612 : Use the lambda parameter instead of capturing the argument 'name'
    
    * Fix Warning S6608 : Indexing at Count-1 should be used instead of the "Enumerable" extension method "Last"
    
    * Set version to '1.20'
    
    ---------
    
    Co-authored-by: Niels Lunn <nilu@clever.dk>
    Co-authored-by: Christian Helle <christian.helle@outlook.com>
    Co-authored-by: Andreas Gehrke <ag@agehrke.com>
    Co-authored-by: Andreas Gehrke <ange@clever.dk>

---
+++ src/Atc.Cosmos.EventStore.Cqrs/ProjectionFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Atc.Cosmos.EventStore.Cqrs;
+
+/// <summary>
+/// Responsible for creating <see cref="IProjection"/> instances.
+/// </summary>
+public interface IProjectionFactory
+{
+    /// <summary>
+    /// Creates a projection of type <typeparamref name="TProjection"/> for the event stream
+    /// identified by <paramref name="streamId"/>.
+    /// </summary>
+    /// <param name="streamId">ID of the stream being projected.</param>
+    /// <param name="cancellationToken">Cancellation.</param>
+    /// <typeparam name="TProjection">Type of projection to create.</typeparam>
+    /// <returns>The created projection.</returns>
+    public Task<IProjection> CreateAsync<TProjection>(EventStreamId streamId, CancellationToken cancellationToken)
+        where TProjection : IProjection;
+}
+
+/// <summary>
+/// The default projection factory which just creates projections by
+/// getting them from the DI-container.
+/// </summary>
+internal sealed class DefaultProjectionFactory : IProjectionFactory
+{
+    private readonly IServiceProvider serviceProvider;
+
+    public DefaultProjectionFactory(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public Task<IProjection> CreateAsync<TProjection>(EventStreamId streamId, CancellationToken cancellationToken)
+        where TProjection : IProjection
+    {
+        return Task.FromResult<IProjection>(serviceProvider.GetRequiredService<TProjection>());
+    }
+}
