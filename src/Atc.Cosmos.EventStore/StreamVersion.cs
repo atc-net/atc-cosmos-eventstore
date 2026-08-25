eb1612d18b434198ed9e2c8ce9e4e71201a30695
Per Kops <per_kops@hotmail.com>
2026-08-25 15:18:34 +0200

    docs(eventstore): fix grammar in StreamVersion doc comment
    - Correct wording in CompareTo remarks section

--- src/Atc.Cosmos.EventStore/StreamVersion.cs
+++ src/Atc.Cosmos.EventStore/StreamVersion.cs
@@ -80,7 +80,7 @@ public struct StreamVersion : IComparable<StreamVersion>, IEquatable<StreamVersi
     ///   A value that indicates the relative order of the objects being compared. The
     ///   return value has these meanings: Value Meaning Less than zero This instance precedes
     ///   other in the sort order. Zero This instance occurs in the same position in the
-    ///   sort order as other. Greater than zero This instance follows other in the sort order.
+    ///   sort order as others. Greater than zero This instance follows other in the sort order.
     /// </returns>
     public int CompareTo(StreamVersion other)
         => Value.CompareTo(other.Value);
